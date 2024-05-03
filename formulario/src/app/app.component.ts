3f4f5048ac1a83f456ffb0d9e497adc452439553
Nathan Ferreira Borges <borges3141@gmail.com>
2024-05-02 21:52:18 -0300

    feat: initial config

--- formulario/src/app/app.component.ts
+++ formulario/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'formulario';
